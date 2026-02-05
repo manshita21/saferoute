bf3af637ee75e150a87a1f383c1ee034a2a25651
Manshita Agarwal <manshita10@gmail.com>
2026-02-05 23:19:19 +0530

    Add community feedback schema

---
+++ backend/src/models/CommunityFeedback.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const communityFeedbackSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        source: {
+            type: String,
+            required: true,
+        },
+        destination: {
+            type: String,
+            required: true,
+        },
+        rating: {
+            type: Number, // 1 = Unsafe, 5 = Safe
+            min: 1,
+            max: 5,
+            required: true,
+        },
+        comment: {
+            type: String,
+        },
+        travelTime: {
+            type: String, // e.g. "23:30"
+        },
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("CommunityFeedback", communityFeedbackSchema);
